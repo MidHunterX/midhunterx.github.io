0d559a354279beaa4e8245334190c4fbc4eb32e0
MidHunterX <midhunterx@gmail.com>
2024-04-16 17:27:27 +0530

    🚀 Trying out routing...

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="flex flex-col items-center justify-between p-24">
+      <h1>Greetings, this is the Dashboard Page!</h1>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
-import Image from "next/image";
+import Link from 'next/link'
 
-export default function Home() {
+export default function Application() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
-      <div>
-
-      </div>
-
-    </main>
+    <div className="flex flex-col items-center justify-between p-24">
+      <h1>Welcome to Main Application</h1>
+      <Link className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" href="/dashboard">Go to Dashboard</Link>
+    </div>
   );
 }
