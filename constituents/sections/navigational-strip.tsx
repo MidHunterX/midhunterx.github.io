de398d03852c72095e7a69ba82705aaf8a6c5266
MidHunterX <midhunterx@gmail.com>
2024-04-17 17:24:24 +0530

    ✈️ Updated: NavBar Theme

--- constituents/sections/navigational-strip.tsx
+++ constituents/sections/navigational-strip.tsx
@@ -10,14 +10,14 @@ export const NavItem = ( { href, text } : NavItemProps ) => (
 
 export const NavItemSelected = ( { href, text } : NavItemProps ) => (
   <li>
-    <a href={href} className="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500" aria-current="page">{text} </a>
+    <a href={href} className="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-yellow-200" aria-current="page">{text} </a>
   </li>
 );
 
 export function NavBar( { children } : { children:any } ) {
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-center mx-auto p-4">
+    <nav className="fixed w-full bg-white border-gray-200 dark:bg-gray-900">
+      <div className="flex flex-wrap items-center justify-center p-4">
         <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
           <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             {children}
