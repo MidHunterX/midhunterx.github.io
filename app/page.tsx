67d9c430fd598d2ab8a8b814498b431ab1ffb784
MidHunterX <midhunterx@gmail.com>
2024-04-17 10:27:41 +0530

    📝 Added: Components into Mainpage

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,54 @@
 // Use { } for Named Exports (without a default)
-import {Button} from '@/constituents/userinterfaces'
+import * as UI from '@/constituents/userinterfaces'
+import {NavItem, NavItemSelected, NavBar} from '@/constituents/sections/navigational-strip'
 
 export default function Application() {
-  return (
-    <div className="flex flex-col items-center justify-between p-24">
-      <h1>Welcome to Main Application</h1>
-      <div className='flex flex-row'>
-        <Button href='/dashboard' text='Go to Dashboard'/>
+  return ( <>
+
+    <NavBar>
+      <NavItemSelected href="#home" text="Home"/>
+      <NavItem href="#home" text="Skills"/>
+      <NavItem href="/dashboard" text="Dashboard"/>
+    </NavBar>
+
+    <UI.Jumbotron
+      head='Greetings, this is Mid Hunter'
+      body='Creating software and automation with ease just for fun.'
+      button1='See More'
+      button2='Hire Me'
+    />
+
+    <UI.BentoBox>
+
+      <UI.Bento1
+        head='How to quickly deploy a static website'
+        body='Static websites are now used to bootstrap lots of websites and
+        are becoming the basis for a variety of tools that even influence
+        both web designers and developers.'
+        button='Read More'
+      />
+
+      <div className="grid md:grid-cols-2 gap-8">
+
+        <UI.Bento2
+          head='Start with Flowbite Design System'
+          body='Static websites are now used to bootstrap lots of websites and
+          are becoming the basis for a variety of tools that even influence
+          both web designers and developers.'
+          button='Read More'
+        />
+
+        <UI.Bento2
+          head='Best react libraries around the web'
+          body='Static websites are now used to bootstrap lots of websites and
+          are becoming the basis for a variety of tools that even influence
+          both web designers and developers.'
+          button='Read More'
+        />
+
       </div>
-    </div>
-  );
+
+    </UI.BentoBox>
+
+  </>);
 }
