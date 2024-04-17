9dc35d2970fcbb9f755a364c1555ecd7fc6aff3e
MidHunterX <midhunterx@gmail.com>
2024-04-17 17:23:15 +0530

    ▶️ Added: Arrow Button and Arrow Link

--- constituents/buttons.tsx
+++ constituents/buttons.tsx
@@ -6,9 +6,34 @@ type ButtonProps = {
 }
 
 export const Button = ({ href, text }:ButtonProps) => (
-  <Link className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900" href={href}> {text} </Link>
+  <Link className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-black rounded-lg bg-yellow-200 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 dark:focus:ring-yellow-900" href={href}> {text} </Link>
 );
 
 export const ButtonSecondary = ({ href, text }:ButtonProps) => (
   <Link className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400" href={href}> {text} </Link>
+);
+
+export const ButtonArrow = ({ href, text }:ButtonProps) => (
+  <a
+    className="inline-flex justify-center items-center py-3 px-5 text-base
+    font-medium text-center text-black rounded-lg bg-blue-500 dark:bg-yellow-200
+    hover:bg-yellow-500 dark:hover:bg-blue-800 focus:ring-4 focus:ring-yellow-300 dark:focus:ring-yellow-900"
+    href={href}>
+    {text}
+    <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+    </svg>
+  </a>
+);
+
+export const LinkArrow = ({ href, text }:ButtonProps) => (
+  <a
+    className="text-blue-600 dark:text-yellow-200 hover:underline font-medium
+    text-lg inline-flex items-center"
+    href={href}>
+    {text}
+    <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+    </svg>
+  </a>
 );
