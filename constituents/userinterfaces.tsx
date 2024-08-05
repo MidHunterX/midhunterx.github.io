ff40ba2c35d3b5418fbf73d5f1b2b38341ef01b2
MidHunterX <midhunterx@gmail.com>
2024-08-05 14:18:36 +0530

    🔧 Fixed: Blobity distracted by Project Items

--- constituents/userinterfaces.tsx
+++ constituents/userinterfaces.tsx
@@ -108,15 +108,11 @@ export const ProjectItem = ({
 
   return (
     <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <a href={href}>
-        <img className="rounded-t-lg" src={img} alt="" />
-      </a>
+      <img className="rounded-t-lg" src={img} alt="" />
       <div className="p-5">
-        <a href={href}>
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {head}
-          </h5>
-        </a>
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          {head}
+        </h5>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
           {body}
         </p>
