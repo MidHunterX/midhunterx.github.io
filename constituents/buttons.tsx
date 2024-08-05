c1b0979aae92a312445ecf1dbc1c6a14102f7bc0
MidHunterX <midhunterx@gmail.com>
2024-08-05 10:10:10 +0530

    🔧 Fixed: Button Spacing

--- constituents/buttons.tsx
+++ constituents/buttons.tsx
@@ -6,11 +6,11 @@ type ButtonProps = {
 }
 
 export const Button = ({ href, text }:ButtonProps) => (
-  <Link className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-black rounded-lg bg-yellow-200 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 dark:focus:ring-yellow-900" href={href}> {text} </Link>
+  <Link className="inline-flex justify-center items-center py-3 px-5 sm:me-4 text-base font-medium text-center text-black rounded-lg bg-yellow-200 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 dark:focus:ring-yellow-900" href={href}> {text} </Link>
 );
 
 export const ButtonSecondary = ({ href, text }:ButtonProps) => (
-  <Link className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400" href={href}> {text} </Link>
+  <Link className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:me-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400" href={href}> {text} </Link>
 );
 
 export const ButtonArrow = ({ href, text }:ButtonProps) => (
