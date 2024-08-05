b45f9f69569ca67f1177b295462b503d1b37572e
MidHunterX <midhunterx@gmail.com>
2024-08-05 10:11:33 +0530

    💼 Added: Project Item Component

--- constituents/userinterfaces.tsx
+++ constituents/userinterfaces.tsx
@@ -1,51 +1,74 @@
-import {Button, ButtonSecondary, ButtonArrow, LinkArrow} from '@/constituents/buttons'
+import {
+  Button,
+  ButtonSecondary,
+  ButtonArrow,
+  LinkArrow,
+} from "@/constituents/buttons";
+
+// ░░█ █░█ █▀▄▀█ █▄▄ █▀█ ▀█▀ █▀█ █▀█ █▄░█
+// █▄█ █▄█ █░▀░█ █▄█ █▄█ ░█░ █▀▄ █▄█ █░▀█
 
 type JumboProps = {
   head: string;
   body: string;
   button1: string;
   button2: string;
-}
+};
 
-export function Jumbotron( { head, body, button1, button2 } : JumboProps ) {
+export function Jumbotron({ head, body, button1, button2 }: JumboProps) {
   return (
     <section className="bg-center bg-no-repeat bg-[url('../public/hero.jpg')] bg-gray-700 bg-blend-multiply">
       <div className="px-4 mx-auto text-center flex flex-col justify-center max-w-screen-xl h-screen py-24 lg:py-56">
-        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">{head}</h1>
-        <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">{body}</p>
+        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
+          {head}
+        </h1>
+        <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">
+          {body}
+        </p>
         <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
-          <Button href='/' text={button1}/>
-          <ButtonSecondary href='/' text={button2}/>
+          <Button href="/" text={button1} />
+          <ButtonSecondary href="/" text={button2} />
         </div>
       </div>
     </section>
   );
 }
 
+// █▄▄ █▀▀ █▄░█ ▀█▀ █▀█ █▄▄ █▀█ ▀▄▀
+// █▄█ ██▄ █░▀█ ░█░ █▄█ █▄█ █▄█ █░█
+
 type BentoProps = {
   head: string;
   body: string;
   href: string;
   button: string;
-}
+};
 
-export const Bento1 = ( { head, body, href, button } : BentoProps ) => (
+export const Bento1 = ({ head, body, href, button }: BentoProps) => (
   <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12 mb-8">
-    <h1 className="text-gray-900 dark:text-white text-3xl md:text-5xl font-extrabold mb-2">{head}</h1>
-    <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-6">{body}</p>
-    <ButtonArrow href={href} text={button}/>
+    <h1 className="text-gray-900 dark:text-white text-3xl md:text-5xl font-extrabold mb-2">
+      {head}
+    </h1>
+    <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-6">
+      {body}
+    </p>
+    <ButtonArrow href={href} text={button} />
   </div>
 );
 
-export const Bento2 = ( { head, body, href, button } : BentoProps ) => (
+export const Bento2 = ({ head, body, href, button }: BentoProps) => (
   <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
-    <h2 className="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">{head}</h2>
-    <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">{body}</p>
-    <LinkArrow href={href} text={button}/>
+    <h2 className="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">
+      {head}
+    </h2>
+    <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">
+      {body}
+    </p>
+    <LinkArrow href={href} text={button} />
   </div>
 );
 
-export function BentoBox( {children} : {children:any} ) {
+export function BentoBox({ children }: { children: any }) {
   return (
     <section className="bg-white dark:bg-gray-900">
       <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16">
@@ -55,26 +78,51 @@ export function BentoBox( {children} : {children:any} ) {
   );
 }
 
+// █▀█ █▀█ █▀█ ░░█ █▀▀ █▀▀ ▀█▀   █ ▀█▀ █▀▀ █▀▄▀█
+// █▀▀ █▀▄ █▄█ █▄█ ██▄ █▄▄ ░█░   █ ░█░ ██▄ █░▀░█
 
-type CardImgProps = {
+type ProjectItemProps = {
   img: string;
   head: string;
   body: string;
-  href: string;
-  button: string;
-}
+  live?: string;
+  code?: string;
+};
 
-export const CardImg = ( { img, head, body, href, button } : CardImgProps ) => (
-  <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <a href={href}>
-      <img className="rounded-t-lg" src={img} alt="" />
-    </a>
-    <div className="p-5">
+export const ProjectItem = ({
+  img,
+  head,
+  body,
+  live,
+  code,
+}: ProjectItemProps) => {
+  let href: string;
+
+  if (live) {
+    href = live;
+  } else if (code) {
+    href = code;
+  } else {
+    href = "/";
+  }
+
+  return (
+    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <a href={href}>
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{head}</h5>
+        <img className="rounded-t-lg" src={img} alt="" />
       </a>
-      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{body}</p>
-      <ButtonArrow href={href} text={button}/>
+      <div className="p-5">
+        <a href={href}>
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {head}
+          </h5>
+        </a>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          {body}
+        </p>
+        {live && <Button href={live} text="View Project" />}
+        {code && <ButtonSecondary href={code} text="Source Code" />}
+      </div>
     </div>
-  </div>
-);
+  );
+};
--- world/workshop.tsx
+++ world/workshop.tsx
@@ -1,83 +1,43 @@
 "use client";
-import * as UI from "@/constituents/userinterfaces";
+import { BentoBox, ProjectItem } from "@/constituents/userinterfaces";
 
 export default function Workshop() {
   return (
     <section id="workshop">
-      <UI.BentoBox>
-        <UI.Bento1
-          head="Familiar with tools and technologies such as"
-          body="<Put a lot of or just some tools and technologies here.> <Put a lot of or just some tools and technologies here.> <Put a lot of or just some tools and technologies here.> <Put a lot of or just some tools and technologies here.> <Put a lot of or just some tools and technologies here.> <Put a lot of or just some tools and technologies here.> <Put a lot of or just some tools and technologies here.>"
-          href="/"
-          button="More Details"
-        />
+      <BentoBox>
+        <h1>Projects</h1>
 
         <div className="grid md:grid-cols-3 gap-8 mb-10">
-          <UI.CardImg
+          <ProjectItem
             img="https://raw.githubusercontent.com/MidHunterX/Played-Games-List/89945f39ae5b996d8a289271567fb8f72670b107/assets/head.png"
             head="Played Games List"
             body="A web development project which help keep create and keep an organized list of all the PC games I have finished playing, while also familiarizing and applying basic web development concepts and best practices."
-            href="https://midhunterx.github.io/Played-Games-List/"
-            button="View Project"
+            live="https://midhunterx.github.io/Played-Games-List/"
+            code="https://github.com/MidHunterX/Played-Games-List"
           />
 
-          <UI.CardImg
+          <ProjectItem
             img="https://raw.githubusercontent.com/MidHunterX/GNOSIS/master/static/sc_queslist.jpg"
             head="GNOSIS"
             body="An ambitious project designed to transform the way individuals engage with and acquire knowledge, fostering a community-driven approach to learning and information exchange."
-            href="https://github.com/MidHunterX/GNOSIS"
-            button="View Code"
+            code="https://github.com/MidHunterX/GNOSIS"
           />
 
-          <UI.CardImg
+          <ProjectItem
             img="https://raw.githubusercontent.com/MidHunterX/Hunter-OS/master/.config/feh/.assets/feh.jpg"
             head="Hunter OS"
             body="A custom-tailored Linux distribution designed specifically for personal use, offering a unique and optimized experience with all the essential operating system features and configurations meticulously curated to meet individual needs."
-            href="https://github.com/MidHunterX/Hunter-OS"
-            button="View Docs"
+            code="https://github.com/MidHunterX/Hunter-OS"
           />
 
-          <UI.CardImg
+          <ProjectItem
             img="https://raw.githubusercontent.com/MidHunterX/NvME/master/img/nvim_record.jpg"
             head="NvME"
             body="This project involves building a personalized NeoVim setup from scratch, incorporating all the desired features and customizations to ensure a perfectly comfortable and efficient development environment tailored to individual preferences."
-            href="https://github.com/MidHunterX/NvME"
-            button="View Docs"
-          />
-
-        </div>
-
-        <div className="grid md:grid-cols-2 gap-8">
-
-          <UI.Bento2
-            head="Project 1"
-            body="This is an innovative web application designed to streamline the process of collaborative project management."
-            href="/"
-            button="Read More"
-          />
-
-          <UI.Bento2
-            head="Project 2"
-            body="This platform offers a comprehensive suite of tools that empower teams to effectively plan, organize, and execute projects of any scale."
-            href="/"
-            button="Read More"
-          />
-
-          <UI.Bento2
-            head="Project 3"
-            body='Key features of "Project 1" include intuitive task management, interactive Gantt charts, real-time collaboration, and customizable reporting.'
-            href="/"
-            button="Read More"
-          />
-
-          <UI.Bento2
-            head="Project 4"
-            body="The platform is designed to enhance team productivity by providing a centralized hub for project communication and documentation."
-            href="/"
-            button="Read More"
+            code="https://github.com/MidHunterX/NvME"
           />
         </div>
-      </UI.BentoBox>
+      </BentoBox>
     </section>
   );
 }
