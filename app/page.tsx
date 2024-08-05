4d13b6d5c7996965e554f3a48aec6a8a78fdbb98
MidHunterX <midhunterx@gmail.com>
2024-08-05 14:04:17 +0530

    🌍 Added: Blobity React Configuration

--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,27 @@ import Workshop from "@/world/workshop";
 
 export default function Application() {
   const blobity = useBlobity({
-    licenseKey: "opensource"
+    licenseKey: "opensource",
+    mode: "normal", // normal, slow, bouncy
+    focusableElementsOffsetX: 5,
+    focusableElementsOffsetY: 5,
+    color: "#e4ded7",
+    // dotColor: "#0e1016", // replaces mouse cursor
+    invert: true,
+    font: "'Montserrat',Segoe UI,Roboto,Helvetica Neue,Arial,sans-serif",
+    fontSize: 16,
+    fontWeight: 500,
+    opacity: 1,
+    fontColor: "#0e1016",
+    zIndex: 100,
+    size: 40,
+    radius: 4,
+    magnetic: true,
   });
   return (
     <>
       <LoadingScreen />
       <FastTravel />
-
       <Home />
       <Workshop />
     </>
