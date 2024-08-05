90550988d82e0e1de3bbcf28ac2371b9ae3a1ed4
MidHunterX <midhunterx@gmail.com>
2024-08-05 20:54:54 +0530

    🏺 Added: Sample Skill Items

--- world/skills.tsx
+++ world/skills.tsx
@@ -4,9 +4,40 @@ export default function Skills() {
   return (
     <section
       id="skills"
-      className="bg-[#161D1F] py-[140px] px-10 flex flex-col gap-20 items-center"
+      className="bg-[#161D1F] pb-[140px] px-10 flex flex-col gap-20 items-center"
     >
-      <h1>Skills</h1>
+      <h2>Skills</h2>
+
+      <div className="w-full grid sm:grid-cols-2  md:grid-cols-3 lg:grid-cols-4  gap-8 text-center py-2 sm:px-0">
+        <div className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg`} >
+          <p>item</p>
+        </div>
+        <div className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg`} >
+          <p>item</p>
+        </div>
+        <div className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg`} >
+          <p>item</p>
+        </div>
+        <div className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg`} >
+          <p>item</p>
+        </div>
+        <div className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg`} >
+          <p>item</p>
+        </div>
+        <div className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg`} >
+          <p>item</p>
+        </div>
+        <div className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg`} >
+          <p>item</p>
+        </div>
+        <div className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg`} >
+          <p>item</p>
+        </div>
+        <div className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg`} >
+          <p>item</p>
+        </div>
+      </div>
+
     </section>
   );
 }
