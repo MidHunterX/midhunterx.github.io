c2e1a80a89f74a70bd5662c936532bb152045027
MidHunterX <midhunterx@gmail.com>
2024-08-06 20:36:38 +0530

    ⚙️ GH Pages Supported Configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
+  // Enable static exports for the App Router.
+  // https://nextjs.org/docs/app/building-your-application/deploying/static-exports
   output: 'export',
+
+  // Deploy under subpath of a domain. This is the slug of your GitHub repository.
+  // https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+  // basePath: "/Portfolio",
+
+  // GitHub uses Jekyll by default to build static pages, and Jekyll ignores all files prefixed with _
+  // https://nextjs.org/docs/app/api-reference/next-config-js/assetPrefix
+  assetPrefix: "./",
+
+  // Disable server-based image optimization. Next.js does not support dynamic features with static exports.
+  // https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
