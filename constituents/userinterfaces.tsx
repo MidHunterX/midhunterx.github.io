675cb4203bd03ea4021d84cac9aaf9c671521df8
MidHunterX <midhunterx@gmail.com>
2024-08-07 23:53:22 +0530

    🔧 Fixed: Uneven Jumbotron Buttons

--- constituents/userinterfaces.tsx
+++ constituents/userinterfaces.tsx
@@ -27,7 +27,7 @@ export function Jumbotron({ head, body, button1, href1, button2, href2 }: JumboP
         <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">
           {body}
         </p>
-        <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+        <div className="flex flex-row justify-center">
           <Button href={href1} text={button1} />
           <ButtonSecondary href={href2} text={button2} />
         </div>
