a2646158be69e3a8bc2b30c971eb0982bad22716
MidHunterX <midhunterx@gmail.com>
2024-08-07 23:54:06 +0530

    🌙 Added: Dark/Light mode Code Button

--- constituents/buttons.tsx
+++ constituents/buttons.tsx
@@ -19,8 +19,10 @@ export const ButtonProjectLive = ({ href, text }:ButtonProps) => (
   </Link>
 );
 
+<button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Dark</button>
+
 export const ButtonProjectCode = ({ href, text }:ButtonProps) => (
-  <Link className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:me-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400" href={href}>
+  <Link className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:me-4 text-base font-medium text-center text-black hover:text-white dark:text-white dark:hover:text-black rounded-lg border border-black dark:border-white hover:bg-gray-900 dark:hover:bg-gray-100 focus:ring-4 focus:ring-gray-600 dark:focus:ring-gray-400" href={href}>
     {text}
   </Link>
 );
