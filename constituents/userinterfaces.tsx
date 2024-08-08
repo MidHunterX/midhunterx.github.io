7d01df3937455e33389e22e7e5ed7726858466b0
MidHunterX <midhunterx@gmail.com>
2024-08-09 01:18:36 +0530

    🔧 Fixed: Better Typography

--- constituents/userinterfaces.tsx
+++ constituents/userinterfaces.tsx
@@ -50,25 +50,27 @@ export function Hero() {
 `;
 
   return (
-    <section className="bg-center bg-no-repeat bg-[url('/hero.jpg')] bg-gray-700 bg-blend-multiply max-w-screen h-screen py-[100px] lg:px-[80px] sm:px-[50px] flex lg:flex-row sm:flex-col items-center">
+    <section className="bg-center bg-no-repeat bg-[url('/hero.jpg')] bg-gray-700 bg-blend-multiply max-w-screen h-screen py-[100px] lg:px-[80px] sm:px-[50px] flex lg:flex-row sm:flex-col gap-20 items-center">
 
-      <div className="w-full lg:w-1/2 px-[50px]">
-        <div className="max-w-fit bg-gray-100 text-gray-800 text-xs font-medium mb-2 px-4 py-1 rounded dark:bg-gray-800 dark:text-gray-200 border border-gray-500">Greetings, my name is</div>
-        <h1 className="mb-4 text-6xl lg:text-8xl md:text-5xl text-white font-extrabold tracking-tight leading-none">
+      <div className="w-full lg:w-2/3 px-[50px]">
+        <div className="max-w-fit bg-gray-100 text-gray-800 text-xs font-medium mb-2 px-4 py-1 rounded dark:bg-gray-800 dark:text-gray-200 border border-gray-500">
+          Greetings, I am known as
+        </div>
+        <h1 className="mb-4 lg:text-8xl sm:text-7xl text-white font-extrabold tracking-tight leading-none">
           Mid Hunter
         </h1>
         <p className="text-lg font-normal text-white lg:text-xl">
           I am a
           <span className="text-yellow-200"> Software Developer</span>
         </p>
-        <p className="my-3 text-gray-400">A Computer Science Engineering graduate passionate about software development. Experienced in creating web applications and innovative solutions to complex problems. Explore my portfolio to see my projects and feel free to connect with me!</p>
+        <p className="my-3 text-gray-400 text-justify">A Computer Science Engineering graduate passionate about software development. Experienced in creating web applications and innovative solutions to complex problems. Explore my portfolio to see my projects and feel free to connect with me!</p>
         <div className="mt-8">
           <Button href="#workshop" text="Get Started" />
           <ButtonSecondary href="#" text="Download CV" />
         </div>
       </div>
 
-      <div className="lg:visible sm:invisible text-white flex flex-col items-center justify-center w-full lg:w-1/2">
+      <div className="lg:visible sm:invisible text-white flex flex-col items-center justify-center w-full lg:w-1/3">
         <pre className="mb-4 font-mono text-sm whitespace-pre-wrap">{art}</pre>
         <p>Might place an image here later maybe 🤷</p>
       </div>
