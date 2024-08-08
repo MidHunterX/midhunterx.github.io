b7a4e737766631feae57d818c9c2362026473a68
MidHunterX <midhunterx@gmail.com>
2024-08-08 08:43:52 +0530

    ❌ Delet: Unwanted Components

--- constituents/buttons.tsx
+++ constituents/buttons.tsx
@@ -6,48 +6,21 @@ type ButtonProps = {
 }
 
 export const Button = ({ href, text }:ButtonProps) => (
-  <Link className="inline-flex justify-center items-center py-3 px-5 sm:me-4 text-base font-medium text-center text-black rounded-lg bg-yellow-200 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 dark:focus:ring-yellow-900" href={href}> {text} </Link>
+  <Link className="inline-flex justify-center items-center py-3 px-5 me-4 text-base font-medium text-center text-black rounded-lg bg-yellow-200 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 dark:focus:ring-yellow-900" href={href}> {text} </Link>
 );
 
 export const ButtonSecondary = ({ href, text }:ButtonProps) => (
-  <Link className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:me-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400" href={href}> {text} </Link>
+  <Link className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 me-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400" href={href}> {text} </Link>
 );
 
 export const ButtonProjectLive = ({ href, text }:ButtonProps) => (
-  <Link className="inline-flex justify-center items-center py-3 px-5 sm:me-4 text-base font-medium text-center text-black rounded-lg bg-yellow-200 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 dark:focus:ring-yellow-900" href={href}>
+  <Link className="inline-flex justify-center items-center py-3 px-5 me-4 text-base font-medium text-center text-black rounded-lg bg-yellow-200 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 dark:focus:ring-yellow-900" href={href}>
     {text}
   </Link>
 );
-
-<button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Dark</button>
 
 export const ButtonProjectCode = ({ href, text }:ButtonProps) => (
-  <Link className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:me-4 text-base font-medium text-center text-black hover:text-white dark:text-white dark:hover:text-black rounded-lg border border-black dark:border-white hover:bg-gray-900 dark:hover:bg-gray-100 focus:ring-4 focus:ring-gray-600 dark:focus:ring-gray-400" href={href}>
+  <Link className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 me-4 text-base font-medium text-center text-black hover:text-white dark:text-white dark:hover:text-black rounded-lg border border-black dark:border-white hover:bg-gray-900 dark:hover:bg-gray-100 focus:ring-4 focus:ring-gray-600 dark:focus:ring-gray-400" href={href}>
     {text}
   </Link>
-);
-
-export const ButtonArrow = ({ href, text }:ButtonProps) => (
-  <a
-    className="inline-flex justify-center items-center py-3 px-5 text-base
-    font-medium text-center text-black rounded-lg bg-blue-500 dark:bg-yellow-200
-    hover:bg-yellow-500 dark:hover:bg-blue-800 focus:ring-4 focus:ring-yellow-300 dark:focus:ring-yellow-900"
-    href={href}>
-    {text}
-    <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-    </svg>
-  </a>
-);
-
-export const LinkArrow = ({ href, text }:ButtonProps) => (
-  <a
-    className="text-blue-600 dark:text-yellow-200 hover:underline font-medium
-    text-lg inline-flex items-center"
-    href={href}>
-    {text}
-    <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-    </svg>
-  </a>
 );
--- constituents/userinterfaces.tsx
+++ constituents/userinterfaces.tsx
@@ -1,8 +1,6 @@
 import {
   Button,
   ButtonSecondary,
-  ButtonArrow,
-  LinkArrow,
 } from "@/constituents/buttons";
 
 // ░░█ █░█ █▀▄▀█ █▄▄ █▀█ ▀█▀ █▀█ █▀█ █▄░█
@@ -19,7 +17,7 @@ type JumboProps = {
 
 export function Jumbotron({ head, body, button1, href1, button2, href2 }: JumboProps) {
   return (
-    <section className="bg-center bg-no-repeat bg-[url('../public/hero.jpg')] bg-gray-700 bg-blend-multiply">
+    <section className="bg-center bg-no-repeat bg-[url('/hero.jpg')] bg-gray-700 bg-blend-multiply">
       <div className="px-4 mx-auto text-center flex flex-col justify-center max-w-screen-xl h-screen py-24 lg:py-56">
         <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
           {head}
@@ -33,47 +31,5 @@ export function Jumbotron({ head, body, button1, href1, button2, href2 }: JumboP
         </div>
       </div>
     </section>
-  );
-}
-
-// █▄▄ █▀▀ █▄░█ ▀█▀ █▀█ █▄▄ █▀█ ▀▄▀
-// █▄█ ██▄ █░▀█ ░█░ █▄█ █▄█ █▄█ █░█
-
-type BentoProps = {
-  head: string;
-  body: string;
-  href: string;
-  button: string;
-};
-
-export const Bento1 = ({ head, body, href, button }: BentoProps) => (
-  <div className="bg-gray-50 dark:bg-[#161D1F] border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12 mb-8">
-    <h1 className="text-gray-900 dark:text-white text-3xl md:text-5xl font-extrabold mb-2">
-      {head}
-    </h1>
-    <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-6">
-      {body}
-    </p>
-    <ButtonArrow href={href} text={button} />
-  </div>
-);
-
-export const Bento2 = ({ head, body, href, button }: BentoProps) => (
-  <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
-    <h2 className="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">
-      {head}
-    </h2>
-    <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">
-      {body}
-    </p>
-    <LinkArrow href={href} text={button} />
-  </div>
-);
-
-export function BentoBox({ children }: { children: any }) {
-  return (
-    <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16">
-      {children}
-    </div>
   );
 }
