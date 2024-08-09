31209fab74aa134207488f06eaf0b1b6b4d13c35
MidHunterX <midhunterx@gmail.com>
2024-08-09 12:07:55 +0530

    💥 Added: Collapsible Responsive ASCII

--- constituents/userinterfaces.tsx
+++ constituents/userinterfaces.tsx
@@ -49,8 +49,30 @@ export function Hero() {
   \\ \\ |________| / /
 `;
 
+  const art2 = `
+.    o.        .o    .
+-\`   yyo      oyy   \`-
+oNo\` yyy      yyy \`oNo
+oMMNsyyy      yyysNMMo
+oMMMMyyy      yyyMMMMo
+oMMhdyyyy.  .yyyyyyMMo
+oMMo yyyMMyyMMyyy oMMo
+oMMo yyysNMMNoyyy oMMo
+oMMo yyy \`++\` yyy oMMo
+oMMo yyy      yyy oMMo
+oMMo oyy      yyo oMMo
+oMMo  .o      o.  oMMo
+oMMo              oMMo
+:NMo              oMN:
+ \`o+              +o\`
+`;
+
   return (
-    <section className="bg-center bg-no-repeat bg-[url('/hero.jpg')] bg-gray-700 bg-blend-multiply max-w-screen h-screen py-[100px] lg:px-[80px] sm:px-[50px] flex lg:flex-row sm:flex-col gap-20 items-center">
+    <section className="bg-center bg-no-repeat bg-[url('/hero.jpg')] bg-gray-700 bg-blend-multiply max-w-screen h-screen py-[100px] lg:px-[80px] sm:px-[50px] lg:flex lg:flex-row items-center">
+
+      <div className="collapse xl:visible lg:visible text-white flex flex-col items-center justify-center w-full lg:w-1/3">
+        <pre className="mb-4 font-mono text-sm whitespace-pre-wrap">{art2}</pre>
+      </div>
 
       <div className="w-full lg:w-2/3 px-[50px]">
         <div className="max-w-fit bg-gray-100 text-gray-800 text-xs font-medium mb-2 px-4 py-1 rounded dark:bg-gray-800 dark:text-gray-200 border border-gray-500">
@@ -64,15 +86,10 @@ export function Hero() {
           <span className="text-yellow-200"> Software Developer</span>
         </p>
         <p className="my-3 text-gray-400 text-justify">A Computer Science Engineering graduate passionate about software development. Experienced in creating web applications and innovative solutions to complex problems. Explore my portfolio to see my projects and feel free to connect with me!</p>
-        <div className="mt-8">
+        <div className="mt-8 flex flex-row">
           <Button href="#workshop" text="Get Started" />
           <ButtonSecondary href="#" text="Download CV" />
         </div>
-      </div>
-
-      <div className="lg:visible sm:invisible text-white flex flex-col items-center justify-center w-full lg:w-1/3">
-        <pre className="mb-4 font-mono text-sm whitespace-pre-wrap">{art}</pre>
-        <p>Might place an image here later maybe 🤷</p>
       </div>
 
     </section>
