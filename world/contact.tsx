f80dbb5eca02ce1ed3056c6bef7c90b414d2608b
MidHunterX <midhunterx@gmail.com>
2024-08-10 15:09:50 +0530

    👤 Added: Textarea + Contact fixes

--- world/contact.tsx
+++ world/contact.tsx
@@ -1,6 +1,26 @@
 "use client";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAddressBook } from "@fortawesome/free-solid-svg-icons";
+import {
+  faAddressBook,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+
+
+type FormItemProps = {
+  label: string;
+  placeholder: string;
+  className?: string;
+};
+
+const ProjectItem = ({ label, placeholder, className }: FormItemProps) => {
+  return (
+    <article className={className}>
+      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"> {label} </label>
+      <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={placeholder} />
+    </article>
+  );
+};
+
 
 export default function Contact() {
   return (
@@ -14,36 +34,32 @@ export default function Contact() {
       </h2>
 
       <form>
-        <div className="grid gap-6 mb-6 md:grid-cols-2">
-          <div>
-            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"> First name </label>
-            <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="John" required />
-          </div>
-          <div>
-            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last name</label>
-            <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Doe" required />
-          </div>
-          <div>
-            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Company</label>
-            <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Company Pvt Ltd." required />
-          </div>
-          <div>
-            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Website URL</label>
-            <input type="url" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="website.com" required />
-          </div>
-        </div>
 
-        <div className="mb-6">
+        <article className="mb-6">
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"> Name </label>
+          <div className="flex">
+            <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border rounded-e-0 border-gray-300 border-e-0 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600"> <FontAwesomeIcon className="px-1" icon={faUser} /> </span>
+            <input required type="text" className="rounded-none rounded-e-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John Doe" />
+          </div>
+        </article>
+
+        <article className="grid gap-6 mb-6 md:grid-cols-2">
+          <ProjectItem label="Company" placeholder="Company Pvt Ltd." />
+          <ProjectItem label="Website" placeholder="website.com" />
+        </article>
+
+        <article className="mb-6">
           <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone number</label>
           <input type="tel" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="+91 1234567890" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required />
-        </div>
+            placeholder="+91 1234567890" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" />
+        </article>
 
-        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+        <article className="mb-6">
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
+          <textarea required rows={4} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
+        </article>
+
+        <button type="submit" className="inline-flex justify-center items-center py-3 px-5 me-4 text-base font-medium text-center text-black rounded-lg bg-yellow-200 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 dark:focus:ring-yellow-900">Submit</button>
       </form>
 
     </section>
