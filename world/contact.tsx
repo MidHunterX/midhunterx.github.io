9295faf587f600cf3a3718ada6e5fc4fa3e7ecde
MidHunterX <midhunterx@gmail.com>
2024-08-10 08:16:16 +0530

    🤙 Added: Contact Form

--- world/contact.tsx
+++ world/contact.tsx
@@ -6,103 +6,46 @@ export default function Contact() {
   return (
     <section
       id="contact"
-      className="px-3 pb-[150px]"
+      className="px-3 md:px-[100px] py-[100px] pb-[150px] flex flex-col gap-20 items-center"
     >
       <h2 className="text-center">
         <FontAwesomeIcon className="pe-4" icon={faAddressBook} />
         Contact
       </h2>
 
-      <form
-        action=""
-        method="POST"
-        className="mx-auto mt-16 max-w-xl sm:mt-20 px-2"
-      >
-        <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+      <form>
+        <div className="grid gap-6 mb-6 md:grid-cols-2">
           <div>
-            <label
-              htmlFor="first-name"
-              className="block text-sm font-semibold leading-6  text-white"
-            >
-              First name
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="text"
-                name="first-name"
-                id="first-name"
-                autoComplete="given-name"
-                className="border text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-[#253134] border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
-                placeholder="First name"
-              />
-            </div>
+            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"> First name </label>
+            <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="John" required />
           </div>
           <div>
-            <label
-              htmlFor="last-name"
-              className="block text-sm font-semibold leading-6  text-white"
-            >
-              Last name
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="text"
-                name="last-name"
-                id="last-name"
-                autoComplete="family-name"
-                className="border text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-[#253134] border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
-                placeholder="Last name"
-              />
-            </div>
+            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last name</label>
+            <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Doe" required />
           </div>
-
-          <div className="sm:col-span-2">
-            <label
-              htmlFor="email"
-              className="block text-sm font-semibold leading-6  text-white"
-            >
-              Email
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="email"
-                name="email"
-                id="email"
-                autoComplete="email"
-                className="border text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-[#253134] border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
-                placeholder="Email"
-              />
-            </div>
+          <div>
+            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Company</label>
+            <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Company Pvt Ltd." required />
           </div>
-
-          <div className="sm:col-span-2">
-            <label
-              htmlFor="message"
-              className="block text-sm font-semibold leading-6 mb-2.5"
-            >
-              Message
-            </label>
-            <div className="w-full mb-4 border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 ">
-              <div className="px-4 py-2 rounded-t-lg bg-[#253134]">
-                <textarea
-                  name="message"
-                  id="message"
-                  className="w-full px-0 text-sm text-gray-900 bg-[#253134] border-0 focus:ring-0 text-white placeholder-gray-400"
-                  placeholder="Write a message"
-                ></textarea>
-              </div>
-              <div className="flex items-center justify-between px-3 py-2 border-t dark:border-gray-600">
-                <button
-                  type="submit"
-                  className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800"
-                >
-                  Send Message
-                </button>
-              </div>
-            </div>
+          <div>
+            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Website URL</label>
+            <input type="url" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="website.com" required />
           </div>
         </div>
+
+        <div className="mb-6">
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone number</label>
+          <input type="tel" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="+91 1234567890" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required />
+        </div>
+
+        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
       </form>
+
     </section>
   );
 }
