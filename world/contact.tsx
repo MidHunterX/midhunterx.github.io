ee77c16d1b42b087bbad13d7f30571c8fae68a81
MidHunterX <midhunterx@gmail.com>
2024-08-13 11:13:42 +0530

    ❌ Delet: Unused Contact Component

--- world/contact.tsx
+++ world/contact.tsx
@@ -9,22 +9,6 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 
-type FormItemProps = {
-  label: string;
-  placeholder: string;
-  className?: string;
-};
-
-const ProjectItem = ({ label, placeholder, className }: FormItemProps) => {
-  return (
-    <article className={className}>
-      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{label}</label>
-      <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={placeholder} />
-    </article>
-  );
-};
-
-
 export default function Contact() {
   const follow_ico_size = 35;
   const follow_ico_class = "fill-gray-800 dark:fill-gray-200";
