142f325772bfdb4559e22034cf04e2581a7b6a98
MidHunterX <midhunterx@gmail.com>
2024-08-14 10:16:52 +0530

    🔧 Fixed: Navbar size on Mobile

--- world/navigation.tsx
+++ world/navigation.tsx
@@ -23,7 +23,7 @@ const NavItem = ({ href, label, text }: NavItemProps) => (
 
 export default function FastTravel() {
   return (
-    <nav className="fixed bottom-0 sm:bottom-8 left-0 right-0 mx-auto flex items-center justify-center gap-1 sm:rounded-lg border border-0 sm:border-2 border-gray-500 bg-neutral-950 px-1 py-1 text-[#e4ded7] backdrop-blur-md w-fit">
+    <nav className="fixed bottom-0 sm:bottom-8 left-0 right-0 mx-auto flex items-center justify-center gap-1 sm:rounded-lg border border-0 sm:border-2 border-gray-500 bg-neutral-950 px-1 py-1 text-[#e4ded7] backdrop-blur-md sm:w-[383.3px] md:p-2 lg:w-fit">
       <Link
         href="MidhunJinan_Resume.pdf"
         target="_blank"
