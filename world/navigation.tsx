3bf7ece13f4ac83ad1d70316470e647cfecd7905
MidHunterX <midhunterx@gmail.com>
2024-08-15 11:41:51 +0530

    ❌ Removed vague View PDF icon from Navbar

--- world/navigation.tsx
+++ world/navigation.tsx
@@ -1,8 +1,6 @@
 "use client";
 import Link from "next/link";
 import { smoothScroll } from '@/world/smoothScroll';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFilePdf } from "@fortawesome/free-solid-svg-icons";
 import { ThemeSwitchNav } from "@/world/themeswitch"
 
 /* ===================== SMOOTH TRAVEL TO DESTINATION ===================== */
@@ -24,19 +22,6 @@ const NavItem = ({ href, label, text }: NavItemProps) => (
 export default function FastTravel() {
   return (
     <nav className="fixed bottom-0 sm:bottom-8 left-0 right-0 mx-auto flex items-center justify-center gap-1 sm:rounded-lg border border-0 sm:border-2 border-gray-500 bg-neutral-950 px-1 py-1 text-[#e4ded7] backdrop-blur-md sm:w-[383.3px] md:p-2 lg:w-fit">
-      <Link
-        href="MidhunJinan_Resume.pdf"
-        target="_blank"
-        className="flex"
-        aria-label="Open my resume"
-        data-blobity-tooltip="View Resume"
-        data-blobity-magnetic="false"
-      >
-        <FontAwesomeIcon
-          className="py-2 px-2 text-[16px] sm:px-4 md:py-1"
-          icon={faFilePdf}
-        />
-      </Link>
 
       <NavItem href="#home" label="Fast Travel to Home" text="Home" />
 
