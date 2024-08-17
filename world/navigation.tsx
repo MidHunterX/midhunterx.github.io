26755b549b21938e3d414b46313814fa586ae78a
MidHunterX <midhunterx@gmail.com>
2024-08-17 09:40:47 +0530

    🧒 Added Child prop to NavItem

--- world/navigation.tsx
+++ world/navigation.tsx
@@ -1,20 +1,21 @@
 "use client";
 import Link from "next/link";
-import { smoothScroll } from '@/world/smoothScroll';
-import { ThemeSwitchNav } from "@/world/themeswitch"
+import { ReactNode } from "react";
+import { smoothScroll } from "@/world/smoothScroll";
+import { ThemeSwitchNav } from "@/world/themeswitch";
 
 /* ===================== SMOOTH TRAVEL TO DESTINATION ===================== */
 
 type NavItemProps = {
   href: string;
   label: string;
-  text: string;
+  children: ReactNode; // ReactNode allows any JSX content
 };
 
-const NavItem = ({ href, label, text }: NavItemProps) => (
+const NavItem = ({ href, label, children }: NavItemProps) => (
   <Link href={href} onClick={smoothScroll} aria-label={label}>
     <p className="text-white m-0 rounded py-2 px-2 sm:px-4 text-[12px] sm:text-[14px] md:py-1 md:px-4">
-      {text}
+      {children}
     </p>
   </Link>
 );
@@ -22,22 +23,21 @@ const NavItem = ({ href, label, text }: NavItemProps) => (
 export default function FastTravel() {
   return (
     <nav className="fixed bottom-0 sm:bottom-8 left-0 right-0 mx-auto flex items-center justify-center gap-1 sm:rounded-lg border border-0 sm:border-2 border-gray-500 bg-neutral-950 px-1 py-1 text-[#e4ded7] backdrop-blur-md sm:w-[383.3px] md:p-2 lg:w-fit">
+      <NavItem href="#home" label="Fast Travel to Home">
+        Home
+      </NavItem>
 
-      <NavItem href="#home" label="Fast Travel to Home" text="Home" />
+      <NavItem href="#workshop" label="Fast Travel to Work Shop">
+        Projects
+      </NavItem>
 
-      <NavItem
-        href="#workshop"
-        label="Fast Travel to Work Shop"
-        text="Projects"
-      />
+      <NavItem href="#skills" label="Scroll to Skills Section">
+        Skills
+      </NavItem>
 
-      <NavItem href="#skills" label="Scroll to Skills Section" text="Skills" />
-
-      <NavItem
-        href="#contact"
-        label="Fast Travel to Postal Service"
-        text="Contact"
-      />
+      <NavItem href="#contact" label="Fast Travel to Postal Service">
+        Contact
+      </NavItem>
 
       <ThemeSwitchNav />
     </nav>
