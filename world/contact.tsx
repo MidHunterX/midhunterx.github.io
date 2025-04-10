0967690582e475c40abeabe7aa62bb1cc33447d5
MidHunterX <midhunterx@gmail.com>
2025-04-10 18:26:02 +0530

    🃏 UI: Center Follow Links
    Signed-off-by: MidHunterX <midhunterx@gmail.com>

--- world/contact.tsx
+++ world/contact.tsx
@@ -3,9 +3,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faAddressBook,
-  faLocationDot,
-  faPhone,
-  faEnvelope,
 } from "@fortawesome/free-solid-svg-icons";
 import SvgGithub from "/public/svg/github.svg";
 import SvgLinkedIn from "/public/svg/linkedin.svg";
@@ -19,7 +16,7 @@ export default function Contact() {
   return (
     <section
       id="contact"
-      className="px-3 md:px-[100px] py-[100px] pb-[150px]"
+      className="px-3 md:px-[100px] h-svh flex flex-col justify-center"
     >
       <h2>
         <FontAwesomeIcon className="pe-4" icon={faAddressBook} />
@@ -27,57 +24,32 @@ export default function Contact() {
       </h2>
 
       <section className="w-fit mx-auto lg:flex gap-20">
-
-        {/* █░░ █▀▀ █▀▀ ▀█▀   █▀ █ █▀▄ █▀▀
-            █▄▄ ██▄ █▀░ ░█░   ▄█ █ █▄▀ ██▄ */}
-        <section className="lg:w-1/2 lg:mx-6 pb-4">
+        <section className="lg:mx-6 pb-4">
           <article className="mb-8">
-            <h3>Contact details</h3>
-            <div className="flex items-start -mx-2">
-              <p className="w-6 h-6 mx-2 text-gray-800 dark:text-gray-100">
-                <FontAwesomeIcon icon={faPhone} />
-              </p>
-              <span className="mx-2 text-gray-700 truncate w-72 dark:text-gray-400">+91 8089-5293-74</span>
-            </div>
-            <div className="flex items-start -mx-2">
-              <p className="w-6 h-6 mx-2 text-gray-800 dark:text-gray-100">
-                <FontAwesomeIcon icon={faEnvelope} />
-              </p>
-              <span className="mx-2 text-gray-700 truncate w-72 dark:text-gray-400">midhunterx@gmail.com</span>
-            </div>
-            <div className="flex items-start -mx-2">
-              <p className="w-6 h-6 mx-2 text-gray-800 dark:text-gray-100">
-                <FontAwesomeIcon icon={faLocationDot} />
-              </p>
-              <span className="mx-2 text-gray-700 truncate w-72 dark:text-gray-400">Pallithura, Kazhakkoottam, Trivandrum, Kerala</span>
-            </div>
-          </article>
-
-          <article className="mb-8">
-            <h3>Follow me on</h3>
-            <div className="flex gap-2 mt-4 -mx-1.5 ">
-              <a className="me-3 mx-1.5 dark:hover:text-blue-400 text-gray-400 transition-colors duration-300 transform hover:text-blue-500"
+            <h3 className="text-center">Follow me on</h3>
+            <div className="flex justify-center gap-6 mt-4">
+              <a className="dark:hover:text-blue-400 text-gray-400 transition-colors duration-300 transform hover:text-blue-500"
                 href="https://github.com/midhunterx"
                 target="_blank"
                 data-blobity-magnetic="false"
               >
                 <SvgGithub className={follow_ico_class} width={follow_ico_size} />
               </a>
-              <a className="me-3 mx-1.5 dark:hover:text-blue-400 text-gray-400 transition-colors duration-300 transform hover:text-blue-500"
+              <a className="dark:hover:text-blue-400 text-gray-400 transition-colors duration-300 transform hover:text-blue-500"
                 href="https://www.linkedin.com/in/midhunterx/"
                 target="_blank"
                 data-blobity-magnetic="false"
               >
                 <SvgLinkedIn className={follow_ico_class} width={follow_ico_size} />
               </a>
-              <a className="me-3 mx-1.5 dark:hover:text-blue-400 text-gray-400 transition-colors duration-300 transform hover:text-blue-500"
+              <a className="dark:hover:text-blue-400 text-gray-400 transition-colors duration-300 transform hover:text-blue-500"
                 href="https://www.instagram.com/mid_hunter"
                 target="_blank"
                 data-blobity-magnetic="false"
               >
                 <SvgInstagram className={follow_ico_class} width={follow_ico_size} />
               </a>
-              <a className="me-3 mx-1.5 dark:hover:text-blue-400 text-gray-400 transition-colors duration-300 transform hover:text-blue-500"
+              <a className="dark:hover:text-blue-400 text-gray-400 transition-colors duration-300 transform hover:text-blue-500"
                 href="https://x.com/Mid_Hunter"
                 target="_blank"
                 data-blobity-magnetic="false"
@@ -86,13 +58,6 @@ export default function Contact() {
               </a>
             </div>
           </article>
-        </section>
-
-
-        {/* █▀█ █ █▀▀ █░█ ▀█▀   █▀ █ █▀▄ █▀▀
-            █▀▄ █ █▄█ █▀█ ░█░   ▄█ █ █▄▀ ██▄ */}
-        <section className="lg:w-1/2 lg:mx-6">
-          <iframe className="rounded-xl focus:outline-none" src={"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31563.759167396533!2d76.84119880712232!3d8.55073150552278!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b05be456a010a99%3A0x749c31577640f200!2sPallithura%2C%20Thiruvananthapuram%2C%20Kerala!5e0!3m2!1sen!2sin!4v1723556639494!5m2!1sen!2sin"} width="360" height="280" loading="lazy"></iframe>
         </section>
       </section>
 
