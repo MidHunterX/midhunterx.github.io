55fd36e79b8258e13780d87e4f977567bc983ce1
MidHunterX <midhunterx@gmail.com>
2026-01-15 08:11:20 +0530

    🎨 ui: add better close mark for case study modal

--- world/projectDev.tsx
+++ world/projectDev.tsx
@@ -1,23 +1,16 @@
 "use client";
-import {
-  Button,
-  ButtonNull,
-  ButtonSecondary,
-  ButtonDisabled,
-  ButtonGroup
-} from "@/constituents/buttons";
+import { Button, ButtonNull, ButtonSecondary, ButtonDisabled, ButtonGroup } from "@/constituents/buttons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBox } from "@fortawesome/free-solid-svg-icons";
-import React, { ReactNode } from 'react';
+import { faBox, faXmark } from "@fortawesome/free-solid-svg-icons";
+import React, { ReactNode } from "react";
 import AnimatedTitle from "@/world/effects/animatedTitle";
 import AnimatedBody from "@/world/effects/animatedBody";
-import Tilt from 'react-parallax-tilt';
+import Tilt from "react-parallax-tilt";
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import dynamic from "next/dynamic";
 import { Dialog, DialogPanel, Transition, TransitionChild } from "@headlessui/react";
 import { Fragment } from "react";
-
 
 // █▀█ █▀█ █▀█ ░░█ █▀▀ █▀▀ ▀█▀   █▀▀ ▄▀█ █▀█ █▀▄
 // █▀▀ █▀▄ █▄█ █▄█ ██▄ █▄▄ ░█░   █▄▄ █▀█ █▀▄ █▄▀
@@ -56,22 +49,25 @@ const ProjectCard = ({ img, head, body, techs, children }: ProjectCardProps) => 
       perspective={1000}
       // scale={1.05}
       transitionSpeed={2000}
-      style={{ transformStyle: 'preserve-3d' }}
+      style={{ transformStyle: "preserve-3d" }}
       className="group max-w-lg bg-white border border-gray-200 rounded-lg shadow dark:bg-[#161D1F] dark:border-gray-700"
     >
       <div className="rounded-t-lg overflow-hidden">
         <Image className="group-hover:scale-110 transition duration-500" src={img} alt="" width={1920} height={1080} />
       </div>
 
-      <div className="p-5" style={{ transformStyle: 'preserve-3d' }}>
-        <div className="px-2" style={{transform: "translateZ(3rem)"}}>
+      <div className="p-5" style={{ transformStyle: "preserve-3d" }}>
+        <div className="px-2" style={{ transform: "translateZ(3rem)" }}>
           <AnimatedTitle text={`${head}`} />
         </div>
         <AnimatedBody text={`${body}`} />
 
         {/* TECHNOLOGIES */}
         {techs && (
-          <ul style={{transform: "translateZ(2rem)"}} className="gap-2 flex flex-wrap max-w-md text-gray-500 dark:text-gray-400">
+          <ul
+            style={{ transform: "translateZ(2rem)" }}
+            className="gap-2 flex flex-wrap max-w-md text-gray-500 dark:text-gray-400"
+          >
             {techs.map((tech, index) => (
               <li key={index} className="technologies rounded-md">
                 {tech}
@@ -80,14 +76,13 @@ const ProjectCard = ({ img, head, body, techs, children }: ProjectCardProps) => 
           </ul>
         )}
         {/* BUTTONS */}
-        <div className="px-3 pb-3" style={{transform: "translateZ(4rem)"}}>
-          { children }
+        <div className="px-3 pb-3" style={{ transform: "translateZ(4rem)" }}>
+          {children}
         </div>
       </div>
     </Tilt>
   );
 };
-
 
 // █▀█ █▀█ █▀█ ░░█ █▀▀ █▀▀ ▀█▀   █ ▀█▀ █▀▀ █▀▄▀█
 // █▀▀ █▀▄ █▄█ █▄█ ██▄ █▄▄ ░█░   █ ░█░ ██▄ █░▀░█
@@ -104,7 +99,7 @@ type ProjectItemProps = {
 
 const ProjectItem = ({ img, head, body, live, code, techs, onOpen }: ProjectItemProps) => {
   return (
-    <ProjectCard img={img} head={head} body={body} techs={techs} >
+    <ProjectCard img={img} head={head} body={body} techs={techs}>
       <ButtonGroup>
         {onOpen && <ButtonNull onClick={onOpen} text="Case Study" />}
         {live && <Button target="_blank" href={live} text="Website" />}
@@ -116,7 +111,7 @@ const ProjectItem = ({ img, head, body, live, code, techs, onOpen }: ProjectItem
 
 const ProjectItemDisabled = ({ img, head, body, live, code, techs, onOpen }: ProjectItemProps) => {
   return (
-    <ProjectCard img={img} head={head} body={body} techs={techs} >
+    <ProjectCard img={img} head={head} body={body} techs={techs}>
       <ButtonGroup>
         {onOpen && <ButtonNull onClick={onOpen} text="Case Study" />}
         {live && <ButtonDisabled text="Currently Viewing" />}
@@ -147,23 +142,19 @@ export default function DeveloperSection() {
   const ContentComponent = activeSlug ? CaseStudyComponents[activeSlug] : null;
 
   return (
-    <section
-      id="developerSection"
-      className="px-3 md:px-[100px] py-[100px]"
-    >
+    <section id="developerSection" className="px-3 md:px-[100px] py-[100px]">
       <h2>
         <FontAwesomeIcon height={48} width={48} className="pe-4" icon={faBox} />
         Projects
       </h2>
 
       <div className="grid w-fit mx-auto sm:px-0 grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-8">
-
         <ProjectItem
           img="project/expression.jpg"
           head="Expression"
           body="Fast, lightweight, rule-based wallpaper daemon for Linux, written in Rust; optimized for minimal system resource usage and source code is built with design patterns for maintainability."
           code="https://github.com/MidHunterX/Expression"
-          techs={['Rust', 'TOML Parsing', 'Process Control']}
+          techs={["Rust", "TOML Parsing", "Process Control"]}
         />
 
         <ProjectItem
@@ -172,7 +163,7 @@ export default function DeveloperSection() {
           body="Personal game library 2.0 built using Angular for tracking and showcasing every game I played throughout my life, complete with details from IGDB API and tracked playtime."
           live="https://midhunterx.github.io/Game-Trackr"
           code="https://github.com/MidHunterX/Game-Trackr"
-          techs={['Angular', 'Figma', 'TypeScript', 'TailwindCSS', 'SASS', 'DaisyUI', 'Python', 'IGDB API']}
+          techs={["Angular", "Figma", "TypeScript", "TailwindCSS", "SASS", "DaisyUI", "Python", "IGDB API"]}
         />
 
         <ProjectItemDisabled
@@ -181,7 +172,7 @@ export default function DeveloperSection() {
           body="This is the webpage you are looking at right now. Developed using Next.js to deepen my understanding of React's core concepts and modern web development practices."
           live="https://midhunterx.github.io"
           code="https://github.com/MidHunterX/midhunterx.github.io"
-          techs={['Next.js', 'React', 'TypeScript', 'TailwindCSS', 'GSAP', 'HeadlessUI', 'Webpack', 'Framer Motion']}
+          techs={["Next.js", "React", "TypeScript", "TailwindCSS", "GSAP", "HeadlessUI", "Webpack", "Framer Motion"]}
         />
 
         <ProjectItem
@@ -190,7 +181,7 @@ export default function DeveloperSection() {
           onOpen={() => openCaseStudy("hunter-os")}
           body="A custom-tailored Linux distribution designed specifically for personal use, offering a unique and optimized experience with all the essential operating system features and configs curated to meet individual needs."
           code="https://github.com/MidHunterX/Hunter-OS"
-          techs={['Wayland', 'SystemD', 'GRUB2', 'dhcpcd', 'Kitty Terminal', 'VIFM', 'Battery Optimized']}
+          techs={["Wayland", "SystemD", "GRUB2", "dhcpcd", "Kitty Terminal", "VIFM", "Battery Optimized"]}
         />
 
         <ProjectItem
@@ -198,7 +189,7 @@ export default function DeveloperSection() {
           head="γνῶσις (GNŌSIS)"
           body="Gnosis is a Greek word that means “knowledge”. This project is a community-driven knowledge base with AI Integration for exploring answers to all of your questions in different media formats."
           code="https://github.com/MidHunterX/GNOSIS"
-          techs={['Django', 'Python', 'Bootstrap', 'GeminiAI', 'FuzzyMatching', 'Markdown Editor', 'Authentication']}
+          techs={["Django", "Python", "Bootstrap", "GeminiAI", "FuzzyMatching", "Markdown Editor", "Authentication"]}
         />
 
         <ProjectItem
@@ -206,7 +197,7 @@ export default function DeveloperSection() {
           head="Scholar CAP"
           body="Scholar CAP (Computer Aided Processing) is a python toolset for data parsing, sanitization, validation and storage of data from docx student scholarship forms to generation of custom formatted Excel sheet for Bank NEFT."
           code="https://github.com/MidHunterX/Scholar-CAP"
-          techs={['Python', 'openpyxl', 'Multi-Threading', 'SQLite3', 'pandas', 'pdfplumber', 'docx']}
+          techs={["Python", "openpyxl", "Multi-Threading", "SQLite3", "pandas", "pdfplumber", "docx"]}
         />
 
         <ProjectItem
@@ -214,7 +205,13 @@ export default function DeveloperSection() {
           head="NvME"
           body="This project involves building a personalized NeoVim setup from scratch, incorporating all the desired features and customizations to ensure a perfectly comfortable and efficient development environment tailored to individual preferences."
           code="https://github.com/MidHunterX/NvME"
-          techs={['Lua', 'TreeSitter Parsing', 'Luasnip Snippet Engine', 'Language Server Protocol', 'CMP Autocompletion']}
+          techs={[
+            "Lua",
+            "TreeSitter Parsing",
+            "Luasnip Snippet Engine",
+            "Language Server Protocol",
+            "CMP Autocompletion",
+          ]}
         />
 
         <ProjectItem
@@ -223,7 +220,7 @@ export default function DeveloperSection() {
           body="A Computer Science Engineering Mini Project of a Webservice implementing Symmetric Encryption/Decryption using the good ol' Caesar Cipher algorithm"
           live="https://midhunterx.github.io/Caesar-Cipher-Cryptography"
           code="https://github.com/MidHunterX/Caesar-Cipher-Cryptography"
-          techs={['HTML', 'CSS', 'Javascript', 'Frequency Analysis', 'Cipher Cracking']}
+          techs={["HTML", "CSS", "Javascript", "Frequency Analysis", "Cipher Cracking"]}
         />
 
         <ProjectItem
@@ -232,9 +229,8 @@ export default function DeveloperSection() {
           body="A web development project which help keep create and keep an organized list of all the PC games I have finished playing, while also familiarizing and applying basic web development concepts and best practices."
           live="https://midhunterx.github.io/Played-Games-List/"
           code="https://github.com/MidHunterX/Played-Games-List"
-          techs={['HTML', 'CSS', 'Javascript']}
+          techs={["HTML", "CSS", "Javascript"]}
         />
-
       </div>
 
       {/* CASE STUDY MODAL */}
@@ -257,8 +253,11 @@ export default function DeveloperSection() {
               <DialogPanel className="w-full max-w-5xl transform overflow-hidden rounded-3xl bg-white dark:bg-[#161D1F] p-8 text-left shadow-xl transition-all">
                 <button
                   onClick={() => setIsOpen(false)}
-                  className="absolute top-4 right-6 text-2xl"
-                >×</button>
+                  className="absolute top-4 right-6 text-2xl px-2 py-1"
+                  data-blobity-magnetic="false"
+                >
+                  <FontAwesomeIcon icon={faXmark} />
+                </button>
 
                 {ContentComponent ? <ContentComponent /> : <p>Loading...</p>}
               </DialogPanel>
